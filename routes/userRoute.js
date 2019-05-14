d75c621783cf633f05dc6301a195223369648e55
Mirangs <berezanskij98@gmail.com>
2019-05-14 07:35:13 +0300

    Fixed bugs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const route = express.Router();
 const executeQuery = require('../libs/db');
 
-route.post('/', (req, res) => {
-  const sql = `SELECT * from user WHERE login = '${req.body.name}'`;
+route.get('/', (req, res) => {
+  const sql = `SELECT * from user WHERE login = '${req.query.name}'`;
   executeQuery(sql).spread(user => {
     res.json(user);
   });
