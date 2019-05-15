4d2e1910501010527f38696f8c08689df6ddb0f9
Mirangs <berezanskij98@gmail.com>
2019-05-16 00:18:29 +0300

    add user post route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const route = express.Router();
 const executeQuery = require('../libs/db');
+const crypto = require('crypto');
 
-route.get('/', (req, res) => {
-  const sql = `SELECT * from user WHERE login = '${req.query.name}'`;
+route.post('/', (req, res) => {
+  const sql = `SELECT * from user WHERE login = '${req.body.name}'`;
   executeQuery(sql).spread(user => {
-    res.json(user);
+    const pass = crypto.createHash('md5').update(req.body.password).digest('hex');
+    if (pass === user[0].pass) {
+      res.json(user);
+    } else {
+      res.status(403);
+      res.json({ status: 'invalid login or password' });
+    }
   });
 });
 
